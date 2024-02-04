bb2e6009c136ddf7c2045b38507ad53648d0fd51
21120036 <thienan2003bt@gmail.com>
2024-02-05 00:37:19 +0700

    initialize and set up for reactjs client UI

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Header
+      <h1>Hello World from THTAn with JWT Auth Manager</h1>
+      Footer
+    </div>
+  );
+}
+
+export default App;
