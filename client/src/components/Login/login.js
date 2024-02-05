13b576a0713f259a935102a678a148af1fda7601
21120036 <thienan2003bt@gmail.com>
2024-02-05 23:54:51 +0700

    initialize the login form UI with bootstrap 5

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -3,9 +3,41 @@ import './login.scss';
 
 function login(props) {
     return (
-        <div className='login container'>
-            <h1>Login form</h1>
-            <button className="btn btn-primary">Click me</button>
+        <div className='login-container mt-3'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='container-left red col-7'>
+                        <div className='brand'>
+                            <h1>
+                                JWT Human Resources Management
+                            </h1>
+                        </div>
+                        <div className='detail'>
+                            <p>
+                                Manage and assign project tasks for personnels all in our unique website.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className='container-right green col-5 d-flex flex-column gap-3 py-3'>
+                        <h1 className='title'>Login Form</h1>
+                        <input type="text" className="form-control" placeholder="Email address or your phone number" />
+                        <input type="password" className="form-control" placeholder="Password" />
+
+                        <div className="text-center">
+                            <button type="submit" className='btn btn-primary'>Login</button>
+                            <div className='my-3'></div>
+                            <a href="/reset-password" className="text-center">Forgot password ?</a>
+                            <hr />
+                        </div>
+                        <div className="text-center">
+
+                            <button type="button" className='btn btn-success'>Create new account</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
